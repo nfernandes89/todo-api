96722e765ebfb14c856d975f2891b5fd49946aa3
nfernandes89 <nelia.fernandes.89@gmail.com>
2023-02-06 22:36:57 +0000

    first commit

---
+++ src/lib/mongo.ts
@@ -0,0 +1,38 @@
+import Hapi from '@hapi/hapi'
+import hapiMongo from 'hapi-mongodb'
+
+/**
+ * `mongo@1.0.0`
+ * - Register the 'hapi-mongodb'
+ */
+export default Object.freeze<Hapi.Plugin<void>>({
+  name: 'mongo',
+  version: '1.0.0',
+  register: async server => {
+
+    const usr = "myUser";
+    const pwd = "ItrLv0yHdkZuN6cH";
+    const options: hapiMongo.Options = {
+      url: `mongodb+srv://${usr}:${pwd}@cluster0.fjwjmb0.mongodb.net/ToDo?retryWrites=true&w=majority`,
+      settings: {useUnifiedTopology: true},
+      decorate: true,
+    }
+
+    await server.register({
+      plugin: hapiMongo,
+      options,
+    })
+  },
+})
+
+declare module '@hapi/hapi' {
+  // extends typeof `Hapi.Server`
+  interface Server {
+    mongo: hapiMongo.HapiMongo
+  }
+
+  // extends typeof `Hapi.Request`
+  interface Request {
+    mongo: hapiMongo.HapiMongo
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import Hapi from '@hapi/hapi'
+
+import pluginLogger from './lib/logger'
+import pluginAuthBasic from './lib/auth-basic'
+import pluginAuthFoo from './lib/auth-foo'
+import pluginMongo from './lib/mongo'
+
+import todo from './routes/todo'
+
+
+/**
+ * Initializes the server (starts the caches, finalizes plugin registration) but does not start
+ * listening on the connection port ({@link Hapi.Server.initialize more}). Use this to get a server
+ * instance for tests ({@link https://hapi.dev/tutorials/testing/?lang=en_US more}).
+ *
+ * @param port {@link Hapi.ServerOptions.port}
+ * @returns singleton instance of server
+ */
+export default async (options?: Hapi.ServerOptions): Promise<Readonly<Hapi.Server>> => {
+
+  const server = Hapi.server(options)
+
+  await Promise.all([
+    server.register(pluginLogger),
+    server.register(pluginAuthBasic),
+    server.register(pluginMongo),
+    server.register(pluginAuthFoo),
+  ])
+
+  await Promise.all([
+    server.register(todo, {routes: {prefix: '/api/todo'}}),
+    
+  ])
+
+  await server.initialize()
+
+  return server
+}
